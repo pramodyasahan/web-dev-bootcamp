3fa20cc00706e7db9cf692f5439098dcb4ceb72a
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-06-13 22:01:26 +0530

    feat: finish app

---
+++ TV-show-search-app/app.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("#searchForm")
+
+form.addEventListener('submit', async function (e) {
+    e.preventDefault()
+    const searchTerm = form.elements.query.value;
+    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`)
+    console.log(res.data)
+})
